2cad711b7fe4d1fccdd934a663b3a118790bb90f
Nioto <antoine.guillemoto@gmail.com>
2017-08-10 21:14:09 +0000

    feat(route) try upload route

--- routes/api/voyage.js
+++ routes/api/voyage.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const Promise = require('bluebird');
+const multer  =   require('multer');
 
 const User = require("../../mongo/schemas/User");
 const Debt = require("../../mongo/schemas/Debt");
 
 const computedDebtRouter = new express.Router();
 
-computedDebtRouter.post("/", (req, res) => {
+const storage =   multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, './uploads');
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.fieldname + '-' + Date.now());
+  }
+});
+const upload = multer({ storage : storage}).single('userPhoto');
+
+
+computedDebtRouter.post("/image", (req, res) => {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.end("Error uploading file.");
+    }
+    res.end("File is uploaded");
+  });
   console.log('toto')
 });
 
