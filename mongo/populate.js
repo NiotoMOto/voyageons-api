8eb8c1d4938b81e759146aa5430deb8bc5e09101
NiotoMOto <antoine.guillemoto@gmail.com>
2017-09-12 18:18:32 +0200

    feat(voyage) add publish & active fields

--- mongo/populate.js
+++ mongo/populate.js
@@ -11,32 +11,34 @@ const Goup = require('./schemas/Group');
 const Voyage = require('./schemas/Voyage');
 
 let saveedUsers = [];
+console.log('toto')
 function populate(next) {
   Promise.all([
-    User.remove({}),
-    Line.remove({}),
-    Debt.remove({}),
+    // User.remove({}),
+    // Line.remove({}),
+    // Debt.remove({}),
     // Voyage.remove({}),
   ]).then(() => (
-    Goup.create({ name: 'goupe 1'})
+    {}
+    // Goup.create({ name: 'goupe 1'})
   )).then(group => (
     Promise.all([ 
-      User.create({ lastName: 'Guillemoto', firstName: 'Antoine', userName: 'Nioto', group }),
-      User.create({ lastName: 'Begain', firstName: 'Johan', userName: 'Arken', group }),
-      User.create({ lastName: 'Cirjean', firstName: 'Remy', userName: 'Arken', group }),
-      User.create({ lastName: 'Fernandez', firstName: 'Sébastien', userName: 'Arken', group }),
+      // User.create({ lastName: 'Guillemoto', firstName: 'Antoine', userName: 'Nioto', group }),
+      // User.create({ lastName: 'Begain', firstName: 'Johan', userName: 'Arken', group }),
+      // User.create({ lastName: 'Cirjean', firstName: 'Remy', userName: 'Arken', group }),
+      // User.create({ lastName: 'Fernandez', firstName: 'Sébastien', userName: 'Arken', group }),
     ])
   )).then(users => {
     saveedUsers = users;
     return Promise.all([ 
-      Line.create({ amount: 66, from: users[0], to: users }),
-      Line.create({ amount: 66, from: users[1], to: users }),
-      Line.create({ amount: 66, from: users[2], to: users }),
-      Line.create({ amount: 66, from: users[3], to: users }),
+      // Line.create({ amount: 66, from: users[0], to: users }),
+      // Line.create({ amount: 66, from: users[1], to: users }),
+      // Line.create({ amount: 66, from: users[2], to: users }),
+      // Line.create({ amount: 66, from: users[3], to: users }),
     ])
   }).then(line => (
     Promise.all([ 
-      Line.create({ amount: 0, from: saveedUsers[2], to: saveedUsers }),
+      // Line.create({ amount: 0, from: saveedUsers[2], to: saveedUsers }),
     ]) 
   ));
 }
--- mongo/schemas/Voyage.js
+++ mongo/schemas/Voyage.js
@@ -7,6 +7,11 @@ const schema = new Schema({
   name: { type: String },
   country: { type: String },
   description:  { type: String },
+  active: { type: Boolean, default: false },
+  published: { type: Boolean, default: false },
+})
+.post('update', function(next) {
+  console.log('%s has been saved', this.doc._id);
 });
 
 module.exports = mongoose.model('Voyage', schema);
